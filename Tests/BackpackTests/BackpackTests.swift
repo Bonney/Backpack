f7820d059fe975152b7439262e282daefd1b7de7
Matt Bonney <bonneym@gmail.com>
2023-08-11 11:14:42 -0400

    Fresh Xcode package setup.

---
+++ Tests/BackpackTests/BackpackTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Backpack
+
+final class BackpackTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Backpack().text, "Hello, World!")
+    }
+}
