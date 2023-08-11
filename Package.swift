1b8f7f1b30f952f9290299a69692ac1ff74fc0cd
Matt Bonney <bonneym@gmail.com>
2023-08-11 11:22:32 -0400

    Folder structure

--- Package.swift
+++ Package.swift
@@ -5,24 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Backpack",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Backpack",
-            targets: ["Backpack"]),
+        .library(name: "Colors", targets: ["Colors"]),
+        .library(name: "Extensions", targets: ["Extensions"]),
+        .library(name: "Views", targets: ["Views"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Backpack",
-            dependencies: []),
-        .testTarget(
-            name: "BackpackTests",
-            dependencies: ["Backpack"]),
+        .target(name: "Colors", dependencies: []),
+        .target(name: "Extensions", dependencies: []),
+        .target(name: "Views", dependencies: []),
     ]
 )
---
+++ Sources/Views/Views.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matt Bonney on 8/11/23.
+//
+
+import Foundation
