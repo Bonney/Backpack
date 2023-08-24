a5b20f9eccd775a44ed1547750cda6b0893d705a
Matt Bonney <bonneym@gmail.com>
2023-08-23 21:38:14 -0400

    Support for spacing items

--- Sources/Backpack/Extensions/SwiftUIText+Appending.swift
+++ Sources/Backpack/Extensions/SwiftUIText+Appending.swift
@@ -2,14 +2,17 @@ import SwiftUI
 
 public extension SwiftUI.Text {
     /// Appends a given String to the end of this Text view.
-    func appending(_ suffix: String) -> Text {
-        return self.appending {
+    func appending(_ suffix: String, withSpace: Bool = false) -> Text {
+        return self.appending(withSpace: withSpace) {
             Text(verbatim: suffix)
         }
     }
 
     /// Appends the given Text view to the end of this Text view.
-    func appending(@ViewBuilder _ suffix: () -> Text) -> Text {
-        return (self + suffix())
+    func appending(withSpace: Bool = false, @ViewBuilder _ suffix: () -> Text) -> Text {
+        if withSpace {
+            return self + Text(" ") + suffix()
+        }
+        return self + suffix()
     }
 }
