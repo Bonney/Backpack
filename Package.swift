471c0eb955c7a218cea5d6542c0c17c6c2957b29
Matt Bonney <bonneym@gmail.com>
2023-11-20 10:39:45 -0500

    Added a old school aqua button style

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "Backpack",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
-        .watchOS(.v9)
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10)
     ],
     products: [
         .library(name: "Backpack", targets: ["Backpack"])
