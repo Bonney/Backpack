d0e5570c4c502404f4105362ff9c3b7981c5bb88
Matt Bonney <bonneym@gmail.com>
2024-12-30 12:19:32 -0500

    Added excellent open-source variable blur effect views

---
+++ Sources/Backpack/Views/VariableBlur/VariableBlurDirection.swift
@@ -0,0 +1,13 @@
+//
+//  VariableBlurDirection.swift
+//  DTerminal
+//
+//  Created by Matt Bonney on 12/23/24.
+//
+
+import Foundation
+
+public enum VariableBlurDirection {
+    case blurredTopClearBottom
+    case blurredBottomClearTop
+}
