231e2520fe423eb6e6528d33ff9e9e38ddb60856
Matt Bonney <bonneym@gmail.com>
2025-05-09 13:24:45 -0400

    Added alt number style extension to Font

--- Sources/Backpack/Extensions/Font.swift
+++ Sources/Backpack/Extensions/Font.swift
@@ -7,16 +7,24 @@
 
 import SwiftUI
 
-public extension Font {
-    static func custom(style: Font.TextStyle, weight: Font.Weight, design: Font.Design) -> Font {
+extension Font {
+    public static func custom(style: Font.TextStyle, weight: Font.Weight, design: Font.Design) -> Font {
         Font.system(style, design: design).weight(weight)
     }
 
-    static func rounded(_ style: Font.TextStyle, weight: Font.Weight? = nil) -> Font {
+    public static func rounded(_ style: Font.TextStyle, weight: Font.Weight? = nil) -> Font {
         if let weight = weight {
             return Font.system(style, design: Design.rounded).weight(weight)
         } else {
             return Font.system(style, design: Design.rounded)
         }
+    }
+
+    public func alternative6and9() -> Font {
+        self._stylisticAlternative(.one)
+    }
+
+    public func alternative4() -> Font {
+        self._stylisticAlternative(.two)
     }
 }
